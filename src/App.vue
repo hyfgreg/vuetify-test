de28aefad04c250d12ffc8b2483493e94ed7c4af
huangyafei <huangyafei@xmov.ai>
2019-12-11 18:51:08 +0800

    udpate

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@
     <app-bar @toggle-side-bar="$refs.SideBar.toggleDrawer()" />
     <side-bar ref="SideBar" />
     <v-content style="height: calc(100vh - 64px)">
-      <router-view />
+      <transition name="fade-transform" mode="out-in">
+        <router-view />
+      </transition>
     </v-content>
     <vue-snotify />
   </v-app>
